6d630a7a51ecaeff22fde17f2403b46fc1ce1eef
uniextra <34218091+uniextra@users.noreply.github.com>
2022-06-20 10:40:50 +0200

    dice

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -19,14 +19,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//
+//        let material = SCNMaterial()
+//
+//        material.diffuse.contents = UIImage(named: "art.scnassets/ship.scn")//UIColor.red
+//
+//        cube.materials = [material]
+//
+//        let node = SCNNode() // un punto en el espacio donde pondremos nuestro elemento
+//
+//        node.position = SCNVector3(0, 0.1, -0.5)
+//
+//        node.geometry = cube
+//
+//
+//        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let Dicescene = SCNScene(named: "art.scnassets/diceCollada.scn")!
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        if let diceNode = Dicescene.rootNode.childNode(withName: "Dice", recursively: true) {
+            
+            diceNode?.position = SCNVector3(0, 0, -0.1)
+            
+            sceneView.scene.rootNode.addChildNode(diceNode)
+        }
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,29 +67,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
